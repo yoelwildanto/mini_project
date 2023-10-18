c4a06994dd8bd18e85526902aea0b42c87b029b9
yoelwildanto <yoel.wildantop@gmail.com>
2023-10-18 18:02:31 +0700

    buat event

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import { Routes,Route } from 'react-router-dom';
 import LandingPage from "./HomePage/Home"
 import Navbar from './Components/Navbar';
 import Discover from './Discover/Discover';
+import CreateEvent from './page/Buat Event';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Routes>
-      <Route path='/' element={<LandingPage/>}/>
+      <Route path='/a' element={<LandingPage/>}/>
       <Route path='/discovery' element={<Discover/>}/>
+      <Route path='/' element={<CreateEvent/>}/>
       </Routes>
     </div>
   );
---
+++ src/page/Buat Event/index.js
@@ -0,0 +1,202 @@
+import React from "react";
+import {
+  Box,
+  VStack,
+  Stack,
+  Checkbox,
+  Button,
+  Image,
+  Text,
+  FormControl,
+  Input,
+  FormLabel,
+  InputGroup,
+  InputRightElement,
+  Center,
+  Spacer,
+  Flex,
+  Select,
+} from "@chakra-ui/react";
+import Footer from "../../HomePage/Footer";
+
+export default function CreateEvent() {
+  const [show, setShow] = React.useState(false);
+  const handleClick = () => setShow(!show);
+
+  return (
+    <Box>
+      <Box
+        p={"1.5em 3.5em"}
+        minH={"100vh"}
+        display={"flex"}
+        alignItems={"center"}
+        justifyContent={"center"}
+        bg={"blue.300"}
+      >
+        <VStack w={"50%"} spacing={"1em"} align={"stretch"}>
+          <Box>
+            <Center>{/* <Image src={logo} w={"50%"} /> */}</Center>
+          </Box>
+          <Box bg={"pink"}>
+            <Center>
+              <Text as={"b"}>CREATE EVENT</Text>
+            </Center>
+          </Box>
+          <Box>
+            <FormControl isRequired>
+              <Box>
+                <FormLabel>Nama Event</FormLabel>
+                <Input
+                //   placeholder="Nama Event"
+                // borderColor={"black"}
+                // _placeholder={{ color: "black" }}
+                // _hover={{ borderColor: "white" }}
+                // _focusVisible={{ borderColor: "white" }}
+                ></Input>
+              </Box>
+              <Box my={"10px"}>
+                <FormLabel>Tanggal</FormLabel>
+                {/* <Input
+                  placeholder="Tanggal Event"
+                  borderColor={"black"}
+                  _placeholder={{ color: "black" }}
+                  _hover={{ borderColor: "white" }}
+                  _focusVisible={{ borderColor: "white" }}
+                ></Input> */}
+                <Input
+                  placeholder="Pilih Tanggal Event"
+                  size="md"
+                  type="date"
+                />
+              </Box>
+              <Box>
+                <Flex>
+                  <Box w={"100%"}>
+                    <FormLabel>Waktu Mulai</FormLabel>
+                    <Input
+                    // borderColor={"black"}
+                    // _hover={{ borderColor: "white" }}
+                    // _focusVisible={{ borderColor: "white" }}
+                    />
+                  </Box>
+                  <Spacer m={".5em"} />
+                  <Box w={"100%"}>
+                    <FormLabel>Waktu Selesai</FormLabel>
+                    <Input
+                    // borderColor={"black"}
+                    // _hover={{ borderColor: "white" }}
+                    // _focusVisible={{ borderColor: "white" }}
+                    />
+                  </Box>
+                </Flex>
+                <Flex my={"10px"}>
+                  <Box w={"100%"}>
+                    {/* <FormLabel>Provinsi</FormLabel>
+                  <Input
+                  borderColor={"black"}
+                  _hover={{ borderColor: "white" }}
+                  _focusVisible={{ borderColor: "white" }}
+                  />
+                </Box>
+                <Spacer m={".5em"} />
+                <Box w={"100%"}>
+                  <FormLabel>Kota</FormLabel>
+                  <Input
+                  borderColor={"black"}
+                  _hover={{ borderColor: "white" }}
+                  _focusVisible={{ borderColor: "white" }}
+                  /> */}
+                    <FormLabel>Kota</FormLabel>
+                    <Select placeholder="Pilih Kota">
+                      <option>Bali</option>
+                      <option>Bandung</option>
+                      <option>Jakarta</option>
+                      <option>Surabaya</option>
+                      <option>Yogyakarta</option>
+                    </Select>
+                  </Box>
+                </Flex>
+                <Box>
+                  <FormLabel>Lokasi</FormLabel>
+                  <Input
+                    placeholder="Lokasi Event"
+                    // borderColor={"black"}
+                    // _placeholder={{ color: "black" }}
+                    // _hover={{ borderColor: "white" }}
+                    // _focusVisible={{ borderColor: "white" }}
+                  ></Input>
+                </Box>
+                <Box my={"10px"}>
+                  <FormLabel>Kategori Tiket</FormLabel>
+                  <Box display={"flex"} flexDir={"row"}>
+                    <Checkbox colorScheme="green" defaultChecked>
+                      Berbayar
+                    </Checkbox>
+                  </Box>
+                  {/* <Spacer /> */}
+                  <Box display={"flex"}>
+                    <Checkbox colorScheme="green" defaultChecked>
+                      Gratis
+                    </Checkbox>
+                  </Box>
+                </Box>
+                <Box>
+                  <FormLabel>Deskripsi</FormLabel>
+                  <Input
+                    placeholder="Deskripsi Event"
+                    borderColor={"black"}
+                    // _placeholder={{ color: "black" }}
+                    _hover={{ borderColor: "white" }}
+                    _focusVisible={{ borderColor: "white" }}
+                  ></Input>
+                </Box>
+                <Box>
+                  <FormLabel>Kategori</FormLabel>
+                  <Select placeholder="Pilih Kategori Event">
+                    <option>Festival</option>
+                    <option>Olahraga</option>
+                    <option>Konser</option>
+                    <option>Teater & Drama</option>
+                    <option>Workshop</option>
+                  </Select>
+                </Box>
+                <Box my={"10px"}>
+                  <FormLabel>Harga Tiket</FormLabel>
+                  <Input
+                    placeholder="Masukkan Harga Tiket"
+                    // borderColor={"black"}
+                    // _placeholder={{ color: "black" }}
+                    // _hover={{ borderColor: "white" }}
+                    // _focusVisible={{ borderColor: "white" }}
+                  ></Input>
+                </Box>
+                {/* <FormLabel>Confirm Password</FormLabel>
+              <InputGroup>
+                <Input
+                  type={show ? "text" : "password"}
+                  placeholder="Confirm Your Password"
+                  // borderColor={"black"}
+                  // _placeholder={{ color: "black" }}
+                  // _hover={{ borderColor: "white" }}
+                  // _focusVisible={{ borderColor: "white" }}
+                ></Input>
+                <InputRightElement width="4em">
+                  <Button size="xs" onClick={handleClick}>
+                    {show ? "Hide" : "Show"}
+                  </Button>
+                </InputRightElement>
+              </InputGroup> */}
+              </Box>
+            </FormControl>
+          </Box>
+          <Box>
+            <Center>
+              <Button>Buat Tiket Event</Button>
+            </Center>
+          </Box>
+        </VStack>
+      </Box>
+      <Footer />
+    </Box>
+  );
+}
