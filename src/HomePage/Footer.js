005af5587ac4ef5ed753e9648cfebd1775d6e21d
yoelwildanto <yoel.wildantop@gmail.com>
2023-10-24 14:25:17 +0700

    up

--- src/HomePage/Footer.js
+++ src/HomePage/Footer.js
@@ -5,9 +5,9 @@ import "../CSS/Footer.css";
 
 const Footer = () => {
   return (
-    <Box mb={"70px"} bg={"white"} height={"100px"}>
-      <Grid templateColumns={"repeat(4, 1fr)"} bg={"#032466"} gap={10}>
-        <GridItem  bg={"red"} ml={"100px"}>
+    <Box pb={"100px"} bg={"white"} height={"100px"}>
+      <Grid className="grid-container">
+        <GridItem className="footer1" bg={"red"} ml={"0px"}>
           <VStack color={"#F6F4F1"}>
             <Text
               mt={"10px"}
@@ -23,7 +23,7 @@ const Footer = () => {
            
           </VStack>
         </GridItem>
-        <GridItem>
+        <GridItem className="footer2">
           <VStack color={"#F6F4F1"}>
             <Text
               mt={"10px"}
@@ -40,7 +40,7 @@ const Footer = () => {
             <Text fontWeight={500} fontSize={"13px"}> FAQ </Text>
           </VStack>
         </GridItem>
-        <GridItem>
+        <GridItem className="footer3">
           <VStack color={"#F6F4F1"}>
             <Text
               mt={"10px"}
@@ -57,7 +57,7 @@ const Footer = () => {
             <Text fontWeight={500} fontSize={"13px"}> Bandung </Text>
           </VStack>
         </GridItem>
-        <GridItem mr={"100px"} mb={"30px"}>
+        <GridItem mr={"auto"} mb={"30px"} className="footer4">
           <VStack color={"#F6F4F1"}>
             <Text
               mt={"10px"}
