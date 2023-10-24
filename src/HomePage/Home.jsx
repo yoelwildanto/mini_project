011453f1f622625cae0a488988bb6b49b29688bd
yoelwildanto <yoel.wildantop@gmail.com>
2023-10-24 14:32:57 +0700

    up

--- src/HomePage/Home.jsx
+++ src/HomePage/Home.jsx
@@ -9,20 +9,71 @@ const LandingPage = () => {
   // const sliderClick = (slider)=>{
   //     alert("hello world");
   //   }
-  const [data, setData] = useState([]);
+  // const [data, setData] = useState([]);
   const [banner, setBanner] = useState([]);
 
+  const data =[
+    {
+      id: 1,
+      name: "Event 1",
+      date: "2023-11-15",
+      ticketPrice: "$25",
+      description: "Company A",
+      image: "../Asset/e.jpg"
+    },
+    {
+      id: 2,
+      name: "Event 2",
+      date: "2023-12-10",
+      ticketPrice: "$30",
+      description: "Company B",
+      image: "../Asset/b.jpg"
+    },
+    {
+      id: 3,
+      name: "Event 3",
+      date: "2023-12-20",
+      ticketPrice: "$20",
+      description: "Company C",
+      image: "../Asset/c.jpg"
+    },
+    {
+      id: 4,
+      name: "Event 4",
+      date: "2023-12-25",
+      ticketPrice: "$40",
+      description: "Company D",
+      image: "../Asset/d.jpg"
+    },
+    {
+      id: 5,
+      name: "Event 5",
+      date: "2023-12-30",
+      ticketPrice: "$35",
+      description: "Company E",
+      image: "../Asset/e.jpg"
+    },
+    {
+      id: 6,
+      name: "Event 6",
+      date: "2024-01-05",
+      ticketPrice: "$28",
+      description: "Company F",
+      image: "../Asset/f.jpg"
+    }
+  ]
+
  
-  useEffect(() => {
-    Axios.get("http://localhost:3001/events")
-      .then((response) => {
-        setData(response.data);
-        // console.log(response.data)
-      })
-      .catch((error) => {
-        console.error("Error fetching data:", error);
-      });
-  }, []);
+  // useEffect(() => {
+  //   Axios.get("http://localhost:3001/events")
+  //     .then((response) => {
+  //       setData(response.data);
+  //       // console.log(response.data)
+  //     })
+  //     .catch((error) => {
+  //       console.error("Error fetching data:", error);
+  //     });
+  // }, []);
 
   useEffect(() => {
     Axios.get("http://localhost:3002/banner")
