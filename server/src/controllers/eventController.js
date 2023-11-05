16ae2c579fe5b0acfbc5a1fec879d19855650846
yoelwildanto <yoel.wildantop@gmail.com>
2023-11-05 19:14:44 +0700

    ganti event controller

--- server/src/controllers/eventController.js
+++ server/src/controllers/eventController.js
@@ -1,54 +1,98 @@
 const {
-    createEventService,
-    findEventsService,
-  } = require("../services/eventService");
-  
-  const findEventsController = async (req, res) => {
-    try {
-      const { province } = req.query;
-      const result = await findEventsService(province);
-      return res.status(200).json({
-        message: "success find",
-        data: result,
-      });
-    } catch (err) {
-      throw err;
-    }
-  };
-  
-  const createEventController = async (req, res) => {
-    try {
-      const {
-        eventName,
-        date,
-        province,
-        city,
-        address,
-        price,
-        eventDescription,
-      } = req.body;
-      console.log(req.body);
-      // const { id } = req.user;
-      const result = await createEventService(
-        eventName,
-        date,
-        province,
-        city,
-        address,
-        price,
-        eventDescription
-      );
-  
-      return res.status(200).json({
-        message: "Success",
-        data: result,
-      });
-    } catch (err) {
-      return res.status(500).send(err?.message);
-    }
-  };
-  
-  module.exports = {
-    createEventController,
-    findEventsController,
-  };
+  createEventService,
+  getCategoryService,
+  getLocationService,
+  getEventsService,
+  getBannerService,
+} = require("../services/eventService");
+
+const createEventController = async (req, res) => {
+  try {
+    const {
+      eventName,
+      categoryId,
+      locationId,
+      userId,
+      startDate,
+      endDate,
+      time,
+      eventStatus,
+      description,
+    } = req.body;
+    const result = await createEventService(
+      eventName,
+      categoryId,
+      locationId,
+      userId,
+      startDate,
+      endDate,
+      time,
+      eventStatus,
+      description,
+      req.file?.filename,
+    );
+    return res.status(200).json({
+      message: "success",
+      data: result,
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send(err.message);
+  }
+};
+
+const getCategoryController = async (req, res) => {
+  try {
+    const result = await getCategoryService();
+    return res.status(200).json({
+      message: "success",
+      data: result,
+    });
+  } catch (err) {
+    throw err;
+  }
+};
+
+const getLocationController = async (req, res) => {
+  try {
+    const result = await getLocationService();
+    return res.status(200).json({
+      message: "success",
+      data: result,
+    });
+  } catch (err) {
+    throw err;
+  }
+};
+
+const getEventsController = async (req, res) => {
+  try {
+    const result = await getEventsService();
+    return res.status(200).json({
+      message: "success",
+      data: result,
+    });
+  } catch (err) {
+    throw err;
+  }
+};
+
+const getBannerController = async (req, res) => {
+  try {
+    const result = await getBannerService();
+    return res.status(200).json({
+      message: "success",
+      data: result,
+    });
+  } catch (err) {
+    throw err;
+  }
+};
+
+module.exports = {
+  createEventController,
+  getCategoryController,
+  getLocationController,
+  getEventsController,
+  getBannerController
+};
