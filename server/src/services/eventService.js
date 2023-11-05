1453db833fc87a0054387313806ccadc4cd6c021
yoelwildanto <yoel.wildantop@gmail.com>
2023-11-05 19:48:08 +0700

    update event service

--- server/src/services/eventService.js
+++ server/src/services/eventService.js
@@ -1,36 +1,72 @@
-const { createEventQuery, findEventsQuery } = require("../queries/eventQuery");
+const {
+  createEventQuery,
+  getCategoryQuery,
+  getLocationQuery,
+  getEventsQuery,
+  getBannerQuery,
+} = require("../queries/eventQuery");
 
-const findEventsService = async (province) => {
+const createEventService = async (
+  eventName,
+  categoryId,
+  locationId,
+  userId,
+  startDate,
+  endDate,
+  time,
+  eventStatus,
+  description,
+  image
+) => {
   try {
-    const res = await findEventsQuery(province);
+    const res = await createEventQuery(
+      eventName,
+      categoryId,
+      locationId,
+      userId,
+      startDate,
+      endDate,
+      time,
+      eventStatus,
+      description,
+      image
+    );
     return res;
   } catch (err) {
     throw err;
   }
 };
 
-const createEventService = async (
-  id,
-  eventName,
-  date,
-  province,
-  city,
-  address,
-  price,
-  eventDescription
-) => {
+const getCategoryService = async () => {
   try {
-    const res = await createEventQuery(
-      id,
-      eventName,
-      date,
-      province,
-      city,
-      address,
-      price,
-      eventDescription
-    );
+    const res = await getCategoryQuery();
+    return res;
+  } catch (err) {
+    throw err;
+  }
+};
 
+const getLocationService = async () => {
+  try {
+    const res = await getLocationQuery();
+    return res;
+  } catch (err) {
+    throw err;
+  }
+};
+
+const getEventsService = async () => {
+  try {
+    const res = await getEventsQuery();
+    return res;
+  } catch (err) {
+    throw err;
+  }
+};
+
+const getBannerService = async () => {
+  try {
+    const res = await getBannerQuery();
     return res;
   } catch (err) {
     throw err;
@@ -39,5 +75,8 @@ const createEventService = async (
 
 module.exports = {
   createEventService,
-  findEventsService,
+  getCategoryService,
+  getLocationService,
+  getEventsService,
+  getBannerService,
 };
